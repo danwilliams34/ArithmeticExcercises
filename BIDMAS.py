82a46b4296eb6772f9627f7d8cd68a868054fabb
danwilliams34 <daniel.williams1@ntlworld.com>
2014-02-05 12:55:33 +0000

    Create BIDMAS.py

---
+++ BIDMAS.py
@@ -0,0 +1,10 @@
+def multiply(a,b):
+    answer =  a*b
+    return (answer)
+print (multiply(5, 3)
+
+def add(a,c)
+    answer =  a*b+c
+    return = (answer)
+print (add(5,3,4))
+#expected outcome is 19#
